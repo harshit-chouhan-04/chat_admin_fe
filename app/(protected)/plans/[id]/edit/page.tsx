ead039d69d44f855e5ed999b130038dc0784ee57
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2026-03-14 22:21:12 +0530

    feat: implement PlanForm component for adding and editing plans; update AddPlanPage and EditPlanPage to utilize the new component

--- app/(protected)/plans/[id]/edit/page.tsx
+++ app/(protected)/plans/[id]/edit/page.tsx
@@ -1,2 +1,6 @@
-export { default } from "../../add/page";
+import { PlanForm } from "@/components/plans/PlanForm";
+
+export default function EditPlanPage({ params }: { params: { id: string } }) {
+	return <PlanForm mode="edit" planId={params.id} />;
+}
 
